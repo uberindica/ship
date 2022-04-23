9dbf92787585b7190dfe68cbd87ca17d4d07e40c
rajdeep nayak <102805294+uberindica@users.noreply.github.com>
2022-04-23 13:20:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1=loadAnimation("ship-1.png","ship-2.png")
+
+  shipImg2 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  shipImg2 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
+
+function draw() {
+  background(0);
+  
+
+  sea.velocityY = 1;
+  sea.velocityY = 0;
+  sea.velocityX = -3;
+  sea.velocityX = - 3;
+  
+
+  
+    
+  drawSprites();
+}
